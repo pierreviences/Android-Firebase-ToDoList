8db22ebb305bafdff242ac8fbe1fddd4adcfdbdc
pierreviences <imaniyazid@gmail.com>
2023-08-18 15:03:14 +0700

    feat: Add Design Layout ADd To Do

---
+++ app/src/main/java/com/example/todolist/fragments/AddTodoPopupFragment.kt
@@ -0,0 +1,35 @@
+package com.example.todolist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todolist.R
+import com.example.todolist.databinding.FragmentAddTodoPopupBinding
+
+class AddTodoPopupFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddTodoPopupBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentAddTodoPopupBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerEvents()
+    }
+
+    private fun registerEvents(){
+        binding.
+    }
+
+
+}
