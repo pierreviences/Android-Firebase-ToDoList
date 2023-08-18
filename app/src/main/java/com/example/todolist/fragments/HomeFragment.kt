1d5202f4f974a1021cdcd21e6270cd3717021cff
pierreviences <imaniyazid@gmail.com>
2023-08-18 15:24:29 +0700

    fix: Fixing Bug Firebase

--- app/src/main/java/com/example/todolist/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/todolist/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.todolist.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -51,7 +52,8 @@ class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListener
     private fun init(view: View){
         navController = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
-        databaseRef = FirebaseDatabase.getInstance().reference.child("Tasks").child(auth.currentUser?.uid.toString())
+        databaseRef = FirebaseDatabase.getInstance().reference
+            .child("Tasks").child(auth.currentUser?.uid.toString())
 
     }
 
